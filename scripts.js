7b23ecb59a250e39e92ef9245a91ec1266155c61
Fania Ra <fania@web.de>
2018-11-23 13:23:57 +0000

    working refactor

--- scripts.js
+++ scripts.js
@@ -2,102 +2,42 @@ const form = document.querySelector("form");
 const list = document.querySelector("ul");
 const tab = document.querySelector("tbody");
 
-let whatInput = document.getElementById("addTodo");
-let whenInput = document.getElementById("addDate");
+const whatInput = document.getElementById("addTodo");
+const whenInput = document.getElementById("addDate");
 
-let todos = [];
-let newtodos = {};
-
+const todos = {};
 let counter = 0;
+
+let today = new Date();
+today = today.toDateString();
+
 
 form.addEventListener("submit", saveTodo);
 function saveTodo() {
   let id = `todo${counter}`;
+  let dueDate = (new Date(whenInput.value)).toDateString();
+  let date = whenInput.value ? dueDate : today;
 
-  newtodos[id] = {
+  todos[id] = {
     what: whatInput.value,
-    when: whenInput.value,
+    when: date,
     done: false
   };
 
-  console.log("new", newtodos);
-
-  // drawTable();
-  drawNewTable();
+  drawTable();
   counter++;
-
   // clear form
-  // whatInput.value = "";
+  whatInput.value = "";
+  whenInput.value = "";
   event.preventDefault();
 }
-
-
-
-
-
-function drawNewTable() {
-  // redraw from scratch
-  tab.innerHTML = "";
-  
-
-  let len = Object.keys(newtodos).length;
-  console.log(len);
-
-  // go through all todos
-  for(const t in newtodos) {
-    
-    // let todo = newtodos[i];
-    console.log(t);
-    console.log(newtodos[t]);
-
-    let tr = document.createElement("tr");
-    let what = document.createElement("td");
-    let when = document.createElement("td");
-    // let done = document.createElement("td");
-    // let edit = document.createElement("td");
-    // let del = document.createElement("td");
-    let doneOrNot = newtodos[t].done ? "checked" : "";
-
-    // set content of table row cells
-    what.innerText = newtodos[t].what;
-    when.innerText = newtodos[t].when;
-    done.innerHTML = `<input type="checkbox" ${doneOrNot}>`;
-    // edit.innerHTML = `<button class="${todo[0]}-edit">Edit</button>`;
-    del.innerHTML = `<button class="${newtodos[t]}-del">Delete</button>`;
-
-    tr.appendChild(what);
-    tr.appendChild(when);
-    // tr.appendChild(done);
-    // tr.appendChild(edit);
-    // tr.appendChild(del);
-    tab.appendChild(tr);
-
-    // update checkbox status
-    // done.addEventListener("change", completeTodo);
-
-    // delete todo
-    // del.addEventListener("click", deleteTodo);
-
-  }
-
-  console.log("draw", newtodos);
-
-  event.preventDefault();
-
-}
-
-
-
 
 
 function drawTable() {
   // redraw from scratch
   tab.innerHTML = "";
-
   // go through all todos
-  for(let i=0; i < todos.length; i++) {
-    
-    let todo = todos[i];
+  for(const t in todos) {
     let tr = document.createElement("tr");
     let thid = document.createElement("td");
     let what = document.createElement("td");
@@ -105,16 +45,14 @@ function drawTable() {
     let done = document.createElement("td");
     let edit = document.createElement("td");
     let del = document.createElement("td");
-    let doneOrNot = todo[3] ? "checked" : "";
-
+    let status = todos[t].done ? "checked" : "";
     // set content of table row cells
-    thid.innerText = todo[0];
-    what.innerText = todo[1];
-    when.innerText = todo[2];
-    done.innerHTML = `<input class="${todo[0]}-check" type="checkbox" ${doneOrNot}>`;
-    edit.innerHTML = `<button class="${todo[0]}-edit">Edit</button>`;
-    del.innerHTML = `<button class="${todo[0]}-del">Delete</button>`;
-
+    thid.innerText = t;
+    what.innerText = todos[t].what;
+    when.innerText = todos[t].when;
+    done.innerHTML = `<input type="checkbox" ${status}>`;
+    edit.innerHTML = `<button>Edit</button>`;
+    del.innerHTML = `<button>Delete</button>`;
     tr.appendChild(thid);
     tr.appendChild(what);
     tr.appendChild(when);
@@ -122,54 +60,32 @@ function drawTable() {
     tr.appendChild(edit);
     tr.appendChild(del);
     tab.appendChild(tr);
-
     // update checkbox status
     done.addEventListener("change", completeTodo);
-
     // delete todo
     del.addEventListener("click", deleteTodo);
-
   }
-
-  console.log("draw", todos);
-
+  // console.log("draw", todos);
   // event.preventDefault();
-
 }
-
 
 
 function completeTodo() {
-  for(let i=0; i < todos.length; i++) {
-    if(`${todos[i][0]}-check` == event.srcElement.className) {
-      todos[i][3] = event.srcElement.checked;
-    }
-  }
+  // checkbox > td > tr
+  const parentRow = event.srcElement.parentElement.parentElement;
+  const rowID = parentRow.firstChild.innerText;
+  // update object and redraw table
+  todos[rowID].done = !todos[rowID].done;
   drawTable();
-  console.log("complete", todos);
 }
-
 
 
 function deleteTodo() {
-  let srcId = event.srcElement.className;
-  console.log("srcId", srcId);
-  let num = srcId.slice(4,5);
-  console.log("num", num);
-  console.log("`${todos[num][0]}-del`", `${todos[num][0]}-del`);
-  // for(let i=0; i < todos.length; i++) {
-  if(`${todos[num][0]}-del` == srcId) {
-    todos.splice(num, 1);
-    // button > td > tr
-    const parentRow = event.srcElement.parentElement.parentElement;
-    // button > td > tr > tbody
-    const parentTBody = event.srcElement.parentElement.parentElement.parentElement;
-    parentTBody.removeChild(parentRow);
-  }
-  // }
+  // button > td > tr
+  const parentRow = event.srcElement.parentElement.parentElement;
+  const rowID = parentRow.firstChild.innerText;
+  // delete from object and redraw table
+  delete todos[rowID];
   drawTable();
-  console.log("delete", todos);
 }
-
-
 
